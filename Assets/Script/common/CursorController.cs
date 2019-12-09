336270e94db1cb4285ce5e46923079fce9339a98
RyanCross <StudioPod.RyanCross@outlook.com>
2019-12-08 18:25:57 -0600

    Messing around with a top-down camera system

---
+++ Assets/Script/common/CursorController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    public Texture2D inactiveCursorTexture;
+    public Texture2D activeCursorTexture;
+    public Player player;
+    public Camera cam;
+    // the max distance the cursor can be from the player and still interact with things
+    public float interactableDistance;
+    bool isCursorActive = false;
+    static Vector2 centerOfCursor = new Vector2(16, 16);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+       // Gizmos.DrawSphere(player.transform.position, 1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cursorWorldPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.DrawLine(cursorWorldPos, player.transform.position, Color.yellow, .5f, false);
+
+
+        if ( Vector2.Distance( new Vector2(player.transform.position.x, player.transform.position.y), new Vector2(cursorWorldPos.x,cursorWorldPos.y)) > interactableDistance)
+        {
+            Cursor.SetCursor(inactiveCursorTexture, centerOfCursor, CursorMode.Auto);
+            isCursorActive = false;
+        }
+        else
+        {
+            Cursor.SetCursor(activeCursorTexture, centerOfCursor, CursorMode.Auto);
+            isCursorActive = true;
+        }
+    }
+}
---
+++ Assets/Script/common/SimplestCamera.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplestCamera : MonoBehaviour
+{
+    public Player player;
+    public Camera camera;
+    public Vector3 lastKnownRelativeMousePosition;
+    public Vector3 lastKnownCursorScreenPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            updateCameraPosition();
+        }
+    }
+
+    void updateCameraPosition()
+    {
+        Vector3 cursorWorldPos = camera.ScreenToWorldPoint(Input.mousePosition);
+        //get distance and direction of cursor (vector) relative to the player.
+        Vector3 relativeMousePos = player.transform.InverseTransformPoint(cursorWorldPos);
+        Debug.Log("Cursor vector relative to player" + relativeMousePos);
+        Debug.DrawLine(player.transform.position, relativeMousePos, Color.yellow, .5f, false);
+
+   
+        if(Input.mousePosition != lastKnownCursorScreenPos)
+        {
+            lastKnownCursorScreenPos = Input.mousePosition;
+            camera.transform.position = relativeMousePos;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, .5f);
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    void calculateLocalCursorVector()
+    {
+        // here "local" means the position of the cursor relative to the player's origin.
+    }
+
+    Vector3 getLocalMousePosition()
+    {
+        Vector3 cursorWorldPos = camera.ScreenToWorldPoint(Input.mousePosition);
+        //get distance and direction of cursor (vector) relative to the player.
+        return player.transform.InverseTransformVector(cursorWorldPos);
+    }
+}
